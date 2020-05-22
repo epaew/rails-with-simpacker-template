c3a48591d92a5780424db59b2fc35ab30a1f78b6
epaew <epaew.333@gmail.com>
2020-05-23 08:47:25 +0900

    Fixup paths in webpack/

--- webpack/jest.config.js
+++ webpack/jest.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   moduleNameMapper: {
-    '^#/(.+)': '<rootDir>/webpack/src/$1',
+    '^#/(.+)': '<rootDir>/src/$1',
   },
   preset: 'ts-jest',
   testEnvironment: 'node',
-  testMatch: ['<rootDir>/webpack/__tests__/**/*.[jt]s?(x)'],
+  testMatch: ['<rootDir>/__tests__/**/*.[jt]s?(x)'],
 };
