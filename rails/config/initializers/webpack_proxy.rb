1e024d077fd7447e3b889e2327edf86dd289d051
epaew <epaew.333@gmail.com>
2020-06-07 08:40:39 +0900

    Revert "Create rails/lib/middlewares/webpack_proxy to proxy requests with path matches `%r{^/packs}`"
    This reverts commit b1ad0ba5c79cf73ce33f4d5da84cf305ebff281c.

--- rails/config/initializers/webpack_proxy.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-
-if Rails.env.development?
-  require Rails.root.join('lib/middlewares/webpack_proxy')
-  Rails.application.config.middleware.insert_before ActionDispatch::Static, WebpackProxy
-end
