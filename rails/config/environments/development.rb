9170c356364754b185e60f07c3f71d3dde6b3e7e
epaew <epaew.333@gmail.com>
2020-06-07 09:37:36 +0900

    Update docker-compose.yaml to use nginx container as reverse proxy

--- rails/config/environments/development.rb
+++ rails/config/environments/development.rb
@@ -54,6 +54,9 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
+  # ActionDispatch::HostAuthorization
+  config.hosts << 'rails'
+
   # Bullet
   config.after_initialize do
     Bullet.enable        = true
